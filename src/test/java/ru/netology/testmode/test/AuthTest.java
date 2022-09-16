affcd2e5036230543a2d6bf3da82586550094160
Igor Khomenko <xomigrek@gmail.com>
2022-09-17 00:00:28 +0300

    Added tests and fixed yml

--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -53,28 +53,43 @@ AuthTest {
     @Test
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
+        Configuration.holdBrowserOpen = true;
         var blockedUser = getRegisteredUser("blocked");
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
         //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+        $("[data-test-id=\"login\"] .input__control").setValue(blockedUser.getLogin());
+        $("[data-test-id=\"password\"] .input__control").setValue(blockedUser.getPassword());
+        $(".button").click();
+        $("[data-test-id=\"error-notification\"] .notification__content").should(text("Ошибка! Пользователь заблокирован"));
     }
 
     @Test
     @DisplayName("Should get error message if login with wrong login")
     void shouldGetErrorIfWrongLogin() {
+        Configuration.holdBrowserOpen = true;
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
         //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
         //  "Пароль" - пользователя registeredUser
+        $("[data-test-id=\"login\"] .input__control").setValue(wrongLogin);
+        $("[data-test-id=\"password\"] .input__control").setValue(registeredUser.getPassword());
+        $(".button").click();
+        $("[data-test-id=\"error-notification\"] .notification__content").should(text("Неверно указан логин или пароль"));
     }
 
     @Test
     @DisplayName("Should get error message if login with wrong password")
     void shouldGetErrorIfWrongPassword() {
+        Configuration.holdBrowserOpen = true;
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
         // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
         //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
         //  "Пароль" - переменную wrongPassword
+        $("[data-test-id=\"login\"] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id=\"password\"] .input__control").setValue(wrongPassword);
+        $(".button").click();
+        $("[data-test-id=\"error-notification\"] .notification__content").should(text("Неверно указан логин или пароль"));
     }
 }
